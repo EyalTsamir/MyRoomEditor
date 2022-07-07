529f6d5b5e29411356663046625e9ba8730e7365
EyalTsamir <eyal.tsamir@gmail.com>
2022-07-07 17:21:50 +0300

    fix
    fix

---
+++ room/src/room/Main.ts
@@ -0,0 +1,33 @@
+
+
+module room {
+
+    export class Main {
+
+        public mMainDiv: HTMLElement;
+        public mCounter: number = 0;
+
+        constructor(iMainDiv: HTMLElement) {
+            this.mMainDiv = iMainDiv;
+            let aDiv = this.mMainDiv.cloneNode(true) as HTMLElement;
+            document.body.appendChild(aDiv);
+            setInterval(() => this.update(aDiv), 1);
+
+        }
+        //______________________________________
+
+        private update(aDiv: HTMLElement) {
+            let aPOS = Math.sin(this.mCounter / 180 * Math.PI) * 200 + 800
+            this.mCounter += 1;
+            this.mMainDiv.innerHTML = "Eyal " + this.mCounter;
+            if (this.mCounter == 1800) {
+                this.mCounter = 0;
+            }
+            aDiv.style.top = aPOS + "px";
+            aDiv.style.left = this.mCounter + "px";
+            aDiv.style.borderColor = "#" + (this.mCounter * this.mCounter).toString(16);
+            aDiv.innerHTML = "" + (this.mCounter * this.mCounter).toString(16) + " - " + (this.mCounter);
+
+        }
+    }
+}
