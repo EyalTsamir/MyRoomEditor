9f537d392481e20b5df8bfdffa0a80fb58379012
ronen tsamir <ronen@allseated.com>
2022-07-09 10:54:55 +0300

    Room3D

--- room/src/room/Room3D.ts
+++ room/src/room/Room3D.ts
@@ -1,37 +1,68 @@
-
-
 namespace room {
 
     export class Room3D {
 
         protected mCamera: THREE.PerspectiveCamera;
         protected mScene: THREE.Scene;
+        protected mRenderer: THREE.WebGLRenderer;
+        // @ts-ignore
+        protected mOrbitControls: THREE.OrbitControls;
 
-        constructor()
-        {
-            const container = document.getElementById('3D_room');
-            const renderer = new THREE.WebGLRenderer({ antialias: true });
-            const aRect = container.getBoundingClientRect();
-            renderer.setSize(aRect.width, aRect.height);
-            container.appendChild(renderer.domElement);
-            const scene = new THREE.Scene();
-            scene.background = new THREE.Color(0xbfe3dd);
-            this.mCamera = new THREE.PerspectiveCamera(40, aRect.width/ aRect.height, 1, 100);
+        protected mCube: THREE.Mesh;
+
+        constructor() {
+            this.createScene();
+            this.createController();
+            this.createRoom();
+            this.animate();
+        }
+        //__________________________________________________________________
+
+        private createScene() {
+            const aContainer = document.getElementById('3D_room');
+            this.mRenderer = new THREE.WebGLRenderer({ antialias: true });
+            const aRect = aContainer.getBoundingClientRect();
+            this.mRenderer.setSize(aRect.width, aRect.height);
+            aContainer.appendChild(this.mRenderer.domElement);
+
+            // ????? ?????
+            this.mScene = new THREE.Scene();
+            this.mScene.background = new THREE.Color(0xbfe3dd);
+            this.mCamera = new THREE.PerspectiveCamera(60, aRect.width / aRect.height, 1, 100);
             this.mCamera.position.set(5, 2, 8);
-            const controls = new (THREE as any).OrbitControls(this.mCamera, renderer.domElement);
-            controls.target.set(0, 0.5, 0);
-            controls.update();
-            controls.enablePan = false;
-            controls.enableDamping = true;
 
+            // ????? ??????
+            const aDirectionalLight = new THREE.DirectionalLight(0xffffff, 1);
+            aDirectionalLight.position.x = 40;
+            aDirectionalLight.position.y = 30;
+            aDirectionalLight.position.z = 20;
+            aDirectionalLight.lookAt(0, 0, 0);
+            this.mScene.add(aDirectionalLight);
         }
-        //______________________________________
+        //___________________________________________________________________________
 
-        private openEditorManager() {
+        private createController() {
+            // @ts-ignore
+            this.mOrbitControls = new (THREE as any).OrbitControls(this.mCamera, this.mRenderer.domElement);
+            this.mOrbitControls.target.set(0, 0.5, 0);
+            this.mOrbitControls.update();
+            this.mOrbitControls.enablePan = false;
+            this.mOrbitControls.enableDamping = true;
         }
+        //___________________________________________________________________________
 
-        
+        private createRoom() {
+            const geometry = new THREE.BoxGeometry(3, 1, 3, 10, 10, 10);
+            const material = new THREE.MeshPhongMaterial();
+            this.mCube = new THREE.Mesh(geometry, material);
+            this.mScene.add(this.mCube);
+        }
+        //___________________________________________________________________________
 
-        
+        private animate() {
+            requestAnimationFrame(() => this.animate());
+            this.mOrbitControls.update();
+            this.mRenderer.render(this.mScene, this.mCamera);
+        }
     }
 }
